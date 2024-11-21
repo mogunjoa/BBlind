634c574d162526bd1dadbaa3bec2643fffbe8e54
seungohjin <corrobolea@gmail.com>
2024-11-21 18:52:04 +0900

    update: 입력 화면 UI 구성

--- app/src/main/java/com/mogun/bblind/present/ui/InputActivity.kt
+++ app/src/main/java/com/mogun/bblind/present/ui/InputActivity.kt
@@ -1,26 +1,64 @@
 package com.mogun.bblind.present.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.mogun.bblind.databinding.ActivityInputBinding
+import com.mogun.bblind.domain.model.Content
+import com.mogun.bblind.present.viewmodel.InputViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class InputActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityInputBinding
+
+    private val viewModel: InputViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInputBinding.inflate(layoutInflater).apply {
             setContentView(root)
-            view = this@InputActivity
+            lifecycleOwner = this@InputActivity
+            viewModel = this@InputActivity.viewModel
         }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        (intent.getSerializableExtra(ITEM) as? Content)?.let {
+            viewModel.initData(it)
+        }
+
+        observeViewModel()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    private fun observeViewModel() {
+        viewModel.doneEvent.observe(this) {
+            Toast.makeText(this, it.second, Toast.LENGTH_SHORT).show()
+
+            if(it.first) {
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val ITEM = "item"
+
+        fun start(context: Context, item: Content? = null) {
+            Intent(context, InputActivity::class.java).apply {
+                putExtra(ITEM, item)
+            }.run {
+                context.startActivity(this)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mogun/bblind/present/viewmodel/InputViewModel.kt
@@ -0,0 +1,39 @@
+package com.mogun.bblind.present.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mogun.bblind.domain.model.Content
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class InputViewModel: ViewModel() {
+
+    private val _doneEvent = MutableLiveData<Pair<Boolean, String>>()
+    val doneEvent: LiveData<Pair<Boolean, String>> = _doneEvent
+
+    var category = MutableLiveData("")
+    var title = MutableLiveData("")
+    var content = MutableLiveData("")
+    private var item: Content? = null
+
+    fun initData(item: Content) {
+        this.item = item
+        category.value = item.category
+        title.value = item.title
+        content.value = item.content
+    }
+
+    fun insertData() {
+        val categoryValue = category.value
+        val titleValue = title.value
+        val contentValue = content.value
+
+        if(categoryValue.isNullOrBlank() || titleValue.isNullOrBlank() || contentValue.isNullOrBlank()) {
+            _doneEvent.value = Pair(false, "모든 항목을 입력하셔야 합니다.")
+            return
+        }
+
+        //TODO API 통신
+    }
+}
